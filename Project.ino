b7bcd6b644ee84f49e986ef6555a9292843b5338
skarj <skaarj.sergey@gmail.com>
2019-02-07 23:22:46 +0300

    extend menu

--- Project.ino
+++ Project.ino
@@ -11,84 +11,116 @@
 #define DFPLAYER_RX 4
 #define DFPLAYER_TX 5
 
-
 GU800 display(DISPLAY_CS, DISPLAY_DC, DISPLAY_RESET);
+unsigned long display_time;
 
 IRrecv irrecv(IR_PIN);
 decode_results results;
-unsigned long key_value = 0;
+unsigned long ir_key_value = 0;
 
 SoftwareSerial SoftwareSerial1(DFPLAYER_RX, DFPLAYER_TX);
 DFRobotDFPlayerMini DFPlayer;
-bool playing = 0;
+
+bool flag_power = 0;
+bool flag_play = 0;
+bool flag_heater = 0;
+bool flag_effect = 0;
 
 void setup() {
   irrecv.enableIRIn();
   irrecv.blink13(true);
   
   display.begin();
-  display.clearDisplay();    // Clear display buffer
+  display.dim(15);                   // Set display brightness (0=minimum, 15=maximum)
+  display.clearDisplay(1);           // Clear framebuffer and blank the display
 
   SoftwareSerial1.begin(9600);
-  delay(1000);
   DFPlayer.begin(SoftwareSerial1);
-  DFPlayer.volume(10);      //Set volume value. From 0 to 30
+  DFPlayer.volume(10);               //Set volume value. From 0 to 30
 }
 
 void loop() {
+
   if (irrecv.decode(&results)){
  
     if (results.value == 0XFFFFFFFF)
-      results.value = key_value;
+      results.value = ir_key_value;
 
     switch(results.value){
       case 0x807F48B7:
-        displaytext(F("Heater"));
+        if (flag_heater == 0){
+          displaytext(F("Heater On"));
+          flag_heater = 1;
+        }
+        else {
+          displaytext(F("Heater Off"));
+          flag_heater = 0;
+        }
       break;
       case 0x807F28D7:
-        displaytext(F("PowerOffOn"));
+        if (flag_power == 0){
+          displaytext(F("Power On"));
+          flag_power = 1;
+        }
+        else {
+          displaytext(F("Power Off"));
+          flag_power = 0;
+        }
       break;
       case 0x807F58A7:
-        displaytext(F("Vol+"));
+        displaytext(F("Vol +"));
         DFPlayer.volumeUp();
       break;
       case 0x807FA05F:
-        displaytext(F("Vol-"));
+        displaytext(F("Vol -"));
         DFPlayer.volumeDown();
       break;
       case 0x807FB847:
-        displaytext(F("Temp+"));
+        displaytext(F("Temp +"));
       break;  
       case 0x807F9867:
-        displaytext(F("Temp-"));
+        displaytext(F("Temp -"));
       break;
       case 0x807F7887:
-        if (playing == 0){
-          displaytext(F("CrackleOn"));
+        if (flag_play == 0){
+          displaytext(F("Crackle On"));
           DFPlayer.enableLoopAll();
-          playing = 1;
+          flag_play = 1;
         }
         else {
-          displaytext(F("CrackleOff"));
+          displaytext(F("Crackle Off"));
           DFPlayer.disableLoopAll();
-          playing = 0;
+          flag_play = 0;
         }
       break;
       case 0x807FF807:
-        displaytext(F("Effect"));
+        if (flag_effect == 0){
+          displaytext(F("Effect On"));
+          flag_effect = 1;
+        }
+        else {
+          displaytext(F("Effect Off"));
+          flag_effect = 0;
+        }
       break;  
     }
-    key_value = results.value;
+    ir_key_value = results.value;
     irrecv.resume(); 
+  }
+
+  if (millis() - display_time > 10000){   // 10 seconds
+    display_time = millis();
+    display.displayOff(1);             // Turn the display off with fade effect (image is preserved)
   }
 }
 
 void displaytext(const __FlashStringHelper* text) {
   display.clearDisplay();             // Clear the buffer
-  display.setTextSize(2);             // Draw 2X-scale text
+  display.setTextSize(3);             // Draw 3X-scale text
   display.setTextColor(WHITE);        // Draw white text
-  display.setCursor(0, 32);           // Start at top-left corner
+  display.setCursor(0, 0);            // Start at top-left corner
   display.println(text);
-  display.display();                  // Show the display buffer on the screen. 
+  display.display();                  // Show the display buffer on the screen.
+  display_time = 0;
 }
 
